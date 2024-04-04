eb64c4028244674b34b0fab3e548b5f4c71bfb06
LyleRomero <lyle.planet@gmail.com>
2024-04-04 07:11:52 -0500

    Se realiza pagina de SignUp con error en el boton de mostrar y ocultar contraseña

---
+++ frontend/src/components/SignUp.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+
+function SignUp() {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: ''
+  });
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes agregar la lógica para enviar los datos de registro al servidor
+    console.log('Formulario enviado:', formData);
+  };
+
+  return (
+    <div>
+      <h2>Registro</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Nombre de usuario:</label>
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Correo electrónico:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+            <label>Contraseña:</label>
+                <input
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                />
+            <button type="button" className="toggle-password-button" onClick={handleTogglePassword}>
+                {showPassword ? "O" : "I"}
+            </button>    
+        </div>
+        <button type="submit">Registrarse</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;
