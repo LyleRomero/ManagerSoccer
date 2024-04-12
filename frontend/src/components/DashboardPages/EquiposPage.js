60aa31538d61538d867743e40a7081aca5c26d31
LyleRomero <lyle.planet@gmail.com>
2024-04-12 14:46:29 -0500

    Se agrega boton de agregar equipo y su formulario

--- frontend/src/components/DashboardPages/EquiposPage.js
+++ frontend/src/components/DashboardPages/EquiposPage.js
@@ -1,15 +1,34 @@
 // EquiposPage.js
-import React from 'react';
+import React, { useState } from 'react';
+
 
 function EquiposPage() {
+
+  const [mostrarFormulario, setMostrarFormulario] = useState(false);
+
+  const toggleFormulario = () => {
+    setMostrarFormulario(!mostrarFormulario);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Lógica para manejar el envío del formulario aquí
+  };
+
   return (
     <div>
-      <p color="black">
-      Equipos del Torneo Equipos Equipos del Torneo Equipos Equipos del Torneo Equipos Equipos del Torneo Equipos 
-      Equipos del Torneo Equipos Equipos del Torneo Equipos Equipos del Torneo Equipos Equipos del Torneo Equipos 
-      Equipos del Torneo Equipos Equipos del Torneo Equipos Equipos del Torneo Equipos Equipos del Torneo Equipos 
-      Equipos del Torneo Equipos Equipos del Torneo Equipos Equipos del Torneo Equipos 
-      </p>
+      <h2>Equipos del torneo</h2>
+      <button onClick={toggleFormulario} className="agregar-btn">Agregar +</button>
+      {mostrarFormulario && (
+        <div className="formulario-container">
+          <h3>Agregar Equipo</h3>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="nombreEquipo">Nombre del Equipo:</label>
+            <input type="text" id="nombreEquipo" />
+            <button type="submit">Aceptar</button>
+          </form>
+        </div>
+      )}
       {/* Aquí puedes agregar más contenido relacionado con los equipos */}
     </div>
   );
