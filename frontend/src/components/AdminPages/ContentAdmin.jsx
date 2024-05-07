a2d0d66693c953badde0736cd84c13da2909d7d6
LyleRomero <lyle.planet@gmail.com>
2024-05-06 22:35:37 -0500

    agregar funcionalidad tarjetas de torneo

--- frontend/src/components/AdminPages/ContentAdmin.jsx
+++ frontend/src/components/AdminPages/ContentAdmin.jsx
@@ -1,38 +1,96 @@
-import React, {useEffect} from "react";
+import React, {useEffect , useState} from "react";
 import { app } from "../firebase/firebaseContext/firebase";
+import { Link } from 'react-router-dom';
 import "./Admin.css"
+import Dashboard from "../Dashboard";
+
 
 function ContentAdmin() {
+
+  const [showForm, setShowForm] = useState(false);
+  const [tournamentName, setTournamentName] = useState('');
+  const [projects, setProjects] = useState([]);
+  const [selectedProjectIndex, setSelectedProjectIndex] = useState(null);
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
+  const handleAddProject = () => {
+    setShowForm(true);
+  };
+
+  const handleCancel = () => {
+    setShowForm(false);
+    setTournamentName('');
+  };
+
+  const handleSubmit = () => {
+    setProjects([...projects, tournamentName]);
+    setShowForm(false);
+    setTournamentName('');
+  };
+
+  
+  const handleDeleteProject = (index) => {
+    setSelectedProjectIndex(index);
+    setShowConfirmation(true);
+  };
+
+  const confirmDeleteProject = () => {
+    projects.splice(selectedProjectIndex, 1);
+    setProjects([...projects]);
+    setShowConfirmation(false);
+  };
+
     return (
       <div class="app-content">
         <div class="projects-section">
           <div class="projects-section-header" style={{"margin-bottom" : "20px" , "display": "flex" , "justify-content" : "space-between"}}>
             <h1>Torneos</h1>
-            <button className="agregar-btn">Agregar +</button>
+            <button className="agregar-btn" onClick={handleAddProject}>Agregar +</button>
           </div>
-          <div class="project-boxes jsGridView" style={{"margin-top" : "20px"}}>
-            <div class="project-box-wrapper">
-              <div class="project-box" style={{ backgroundColor: "#fee4cb" }}>
-                <div class="project-box-header" style={{"display": "flex" , "justify-content" : "space-between"}}>
-                  <span style={{ marginRight: 'auto' }}>Fecha 00/00/00</span>
-                  <div class="more-wrapper">
-                    <button class="project-btn-more">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical">
-                        <circle cx="12" cy="12" r="1" />
-                        <circle cx="12" cy="5" r="1" />
-                        <circle cx="12" cy="19" r="1" /></svg>
-                    </button>
+          <div class="project-boxes jsGridView" style={{"margin-top" : "20px" , "marginBottom" : "20px" }}>
+            {projects.map((project, index) => (
+              <div class="project-box-wrapper" style={{ "marginBottom" : "10px" , "marginRight" : "10px"}}>
+                <div class="project-box">
+                  <div class="project-box-header" style={{ "display" : "flex", "justifyContent" : "flex-end" }}>
+                    <div class="more-wrapper">
+                      <button class="project-btn-more" onClick={() => handleDeleteProject(index)}>X</button>
+                    </div>
                   </div>
+                  <Link to="/dashboard/*" key={index}>
+                    <div class="project-box-content-header">
+                      <p class="box-content-header">{project}</p>
+                    </div>
+                  </ Link>
                 </div>
-                <div class="project-box-content-header">
-                  <p class="box-content-header">Torneo #1</p>
-                </div>
+              </div>
+            ))}
+          </div>
+          {showForm && (
+          <div className="form-popup">
+            <form onSubmit={handleSubmit} className="form-container-popup">
+              <h2>Agregar Nuevo Torneo</h2>
+              <input type="text" placeholder="Nombre del Torneo" value={tournamentName} onChange={(e) => setTournamentName(e.target.value)} />
+              <div className="button-group">
+                <button type="button" className="cancel-button" onClick={handleCancel}>Cancelar</button>
+                <button type="submit" className="submit-button">Aceptar</button>
+              </div>
+            </form>
+          </div>
+          )}
+          {showConfirmation && (
+          <div className="form-popup">
+            <div className="form-container-popup">
+              <p>¿Estás seguro de que deseas eliminar este Torneo?</p>
+              <div className="button-group">
+                <button className="cancel-button" onClick={() => setShowConfirmation(false)}>Cancelar</button>
+                <button className="delete-button" onClick={confirmDeleteProject}>Eliminar</button>
               </div>
             </div>
           </div>
+          )}
         </div>
       </div>
-    )
+    );
   };
 
 export default ContentAdmin;
